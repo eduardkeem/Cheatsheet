0bbff6b0b9c5cf42d78d514c4a0c338fecbf35fe
Eduard <eduard.keem@gmail.com>
2022-08-29 22:36:59 +0600

    27.8.22

--- JavaScript/2-middle.js
+++ JavaScript/2-middle.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const adder = (initial = 0) => ({
+const adder = (initial = 0) => ({  // если убрать круглые скобки, будет блок операторов, а так обьект
   value: initial,
-  steps: [initial],
+  steps: [initial],          // значение массив
   add(value) {
     this.steps.push(value);
     this.value += value;
-    return this;
+    return this;             // Вернет ссылку на этот же объект
   }
 });
 
-const Adder = class {
-  constructor(initial = 0) {
+const Adder = class {         // Классы с большой буквы
+  constructor(initial = 0) {  // Конструктор функция внутри класса, которая возвращает только объект
     this.steps = [initial];
     this.value = initial;
   }
@@ -25,15 +25,16 @@ const Adder = class {
 
 // Usage
 
-{
-  const { value, steps } = adder(5).add(-8).add(11);
+{   // Вложенный блок операторов, возможно в пределах фигурных скобок создать переменную с таким же идентификатором (использовать одну и ту-же переменную с разными)
+
+  const { value, steps } = adder(5).add(-8).add(11); // У переменной два оператора - деструктивное присвоение
   console.log(value);
   const [a, b, c] = steps;
-  console.log(a, b, c);
+  console.log(a, b, c);          // Нельзя отобразить только одну переменную, т. к. у нас три значения
 }
 
 {
-  const { value, steps } = new Adder(5).add(-8).add(11);
+  const { value, steps } = new Adder(5).add(-8).add(11); // Создание нового класса
   console.log(value);
   const [a, b, c] = steps;
   console.log(a, b, c);
